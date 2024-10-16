a76c7ba965d14b4ee32554fc93e9d7b33601c1a9
tanimtorikul <torikulswe@gmail.com>
2024-10-16 08:46:44 +0600

    fixed the error of patch user api

--- index.js
+++ index.js
@@ -58,14 +58,13 @@ async function run() {
 
     app.patch("/users/admin/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = { _id: new ObjectId() };
+      const filter = { _id: new ObjectId(id) };
       const updatedDoc = {
         $set: {
           role: "admin",
         },
       };
-      const result = await usersColletion.updatedDoc(filter, updatedDoc);
-      res.send(result);
+      const result = await usersCollection.updateOne(filter, updatedDoc);
     });
 
     // get all products
